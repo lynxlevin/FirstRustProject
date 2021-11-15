4b5cf4daff9ffc4ab29a0fdfc5a4fb48abb57a08
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2021-11-15 16:08:31 +0900

    blog: implementing OOP's state pattern

---
+++ state_pattern_workspace/blog_main/src/main.rs
@@ -0,0 +1,17 @@
+use blog::Post;
+
+fn main() {
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+    println!("OK!");
+
+    post.request_review();
+    assert_eq!("", post.content());
+    println!("OK!");
+
+    post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
+    println!("OK!");
+}
