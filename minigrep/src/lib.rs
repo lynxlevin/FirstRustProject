a9ec4580e9410a065749b7b09dfe2d7e2497de25
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2021-11-20 21:28:18 +0900

    TDD add search method
    + add tests for search method
    + add search method that returns lines containing query
    + add call to search method into run method
    
    https://doc.rust-lang.org/book/ch12-05-working-with-environment-variables.html

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -22,7 +22,57 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
   let contents = fs::read_to_string(config.filename)?;
 
-  println!("With text:\n{}", contents);
+  for line in search(&config.query, &contents) {
+    println!("{}", line);
+  }
 
   Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+  contents
+    .lines()
+    .filter(|line| line.contains(query))
+    .collect()
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn one_result() {
+    let query = "duct";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+    assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+  }
+
+  #[test]
+  fn multiple_results() {
+    let query = ".";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+    assert_eq!(
+      vec!["safe, fast, productive.", "Pick three."],
+      search(query, contents)
+    );
+  }
+
+  #[test]
+  fn no_duplicate_lines() {
+    let query = ", ";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+    assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+  }
 }
